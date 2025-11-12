94390d2f702d1c0b6a26f7ac11a691ef0a83a620
Matias0910 <chamy_241@hotmail.com>
2025-11-11 23:32:50 -0300

    Add Not Found Middleware

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
+import notFound from "./src/middlewares/not-found.js";
 const app = express();
 
+app.use((req, res, next) => {
+    // res.json({ message: "Api en mantenimiento" });
+    console.log(req.method);
+    next();
+});
+
 app.get("/", (req, res) => {
-    res.send("Bienvenido a mi API REST con Express!");
-})
+    res.json({ message: "Bienvenido a mi API REST con Node.js y Firebase" });
+});
+
+app.use(notFound);
 
 const PORT = 3000;
 
